3d6bae386ef06e2d359333d3b622b1ebdb3168c7
Michael Muré <batolettre@gmail.com>
2014-03-10 20:56:28 +0100

    expand the donate tab to show the qr code and a link to open a wallet

--- src/ninja.donatetab.js
+++ src/ninja.donatetab.js
@@ -5,5 +5,11 @@ ninja.wallets.donate = {
 
     close: function () {
         document.getElementById("donatearea").style.display = "none";
+    },
+    
+    displayQrCode: function (currencyid) {
+        var keyValuePair = {};
+    	keyValuePair["donateqrcode"] = janin.currencies[currencyid].donate;
+		ninja.qrCode.showQrCode(keyValuePair, 4);
     }
 };
--- src/ninja.onload.js
+++ src/ninja.onload.js
@@ -25,7 +25,11 @@ select.innerHTML = options;
 var donatelist = document.getElementById("donatelist");
 var list = "<table>";
 for(i = 0; i < janin.currencies.length; i++) {
-    list += "<tr><td>"+janin.currencies[i].name+"</td><td class='address'>"+janin.currencies[i].donate+"</td></tr>";
+    list += "<tr onmouseover='ninja.wallets.donate.displayQrCode("+i+")'>"
+        +"<td>"+janin.currencies[i].name+"</td>"
+        +"<td class='address'><a href='"+janin.currencies[i].name.toLowerCase()+":"+janin.currencies[i].donate+"'>"
+        +janin.currencies[i].donate+"</a></td></tr>";
 }
 list += "</table>";
 donatelist.innerHTML = list;
+
