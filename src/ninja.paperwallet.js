756e5bccb0834190e782f88cf021b75bd2db3cd3
Lucas Legname <lucas.legname@gmail.com>
2014-03-31 23:21:53 +0200

    Useless images have been deleted, wallet style is now dynamic.

--- src/ninja.paperwallet.js
+++ src/ninja.paperwallet.js
@@ -134,10 +134,11 @@ ninja.wallets.paperwallet = {
 			keyelement = 'btcencryptedkey'
 		
 		var coinImgUrl = "logos/" + janin.selectedCurrency.name + ".png";
+		var walletBackgroundUrl = "wallets/" + janin.selectedCurrency.name + ".png";
 
 		var walletHtml =
 							"<div class='coinIcoin'> <img id='coinImg' src='" + coinImgUrl + "' alt='currency_logo' /></div><div class='artwallet' id='artwallet" + i + "'>" +
-								"<img id='papersvg" + i + "' class='papersvg' src='images/bitcoin_crypto_wallet.png' />" +
+								"<img id='papersvg" + i + "' class='papersvg' src='" + walletBackgroundUrl + "' />" +
 								"<div id='qrcode_public" + i + "' class='qrcode_public'></div>" +
 								"<div id='qrcode_private" + i + "' class='qrcode_private'></div>" +
 								"<div class='btcaddress' id='btcaddress" + i + "'></div>" +
