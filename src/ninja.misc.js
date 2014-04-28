42e9855a08b494570bbc1aa330da5e34fe865aa8
Michael Muré <batolettre@gmail.com>
2014-04-28 23:05:51 +0200

    Add a skip button for the seeding and a progress bar, to be styled

--- src/ninja.misc.js
+++ src/ninja.misc.js
@@ -1,97 +1,105 @@
-ninja.seeder = {
-	init: (function () {
-		document.getElementById("generatekeyinput").value = "";
-	})(),
+	ninja.seeder = {
+		init: (function () {
+			document.getElementById("generatekeyinput").value = "";
+		})(),
 
-	// number of mouse movements to wait for
-	seedLimit: (function () {
-		var num = Crypto.util.randomBytes(12)[11];
-		return 200 + Math.floor(num);
-	})(),
+		// number of mouse movements to wait for
+		seedLimit: (function () {
+			var num = Crypto.util.randomBytes(12)[11];
+			return 200 + Math.floor(num);
+		})(),
 
-	seedCount: 0, // counter
-	lastInputTime: new Date().getTime(),
-	seedPoints: [],
+		seedCount: 0, // counter
+		lastInputTime: new Date().getTime(),
+		seedPoints: [],
 
-	// seed function exists to wait for mouse movement to add more entropy before generating an address
-	seed: function (evt) {
-		if (!evt) var evt = window.event;
-		var timeStamp = new Date().getTime();
-		// seeding is over now we generate and display the address
-		if (ninja.seeder.seedCount == ninja.seeder.seedLimit) {
-			ninja.seeder.seedCount++;
-			ninja.wallets.singlewallet.open();
-			document.getElementById("generate").style.display = "none";
-			document.getElementById("menu").style.visibility = "visible";
-			ninja.seeder.removePoints();
-		}
-		// seed mouse position X and Y when mouse movements are greater than 40ms apart.
-		else if ((ninja.seeder.seedCount < ninja.seeder.seedLimit) && evt && (timeStamp - ninja.seeder.lastInputTime) > 40) {
-			SecureRandom.seedTime();
-			SecureRandom.seedInt16((evt.clientX * evt.clientY));
-			ninja.seeder.showPoint(evt.clientX, evt.clientY);
-			ninja.seeder.seedCount++;
-			ninja.seeder.lastInputTime = new Date().getTime();
-			ninja.seeder.showPool();
-		}
-	},
-
-	// seed function exists to wait for mouse movement to add more entropy before generating an address
-	seedKeyPress: function (evt) {
-		if (!evt) var evt = window.event;
-		// seeding is over now we generate and display the address
-		if (ninja.seeder.seedCount == ninja.seeder.seedLimit) {
-			ninja.seeder.seedCount++;
-			ninja.wallets.singlewallet.open();
-			document.getElementById("generate").style.display = "none";
-			document.getElementById("menu").style.visibility = "visible";
-			ninja.seeder.removePoints();
-		}
-		// seed key press character
-		else if ((ninja.seeder.seedCount < ninja.seeder.seedLimit) && evt.which) {
+		// seed function exists to wait for mouse movement to add more entropy before generating an address
+		seed: function (evt) {
+			if (!evt) var evt = window.event;
 			var timeStamp = new Date().getTime();
-			// seed a bunch (minimum seedLimit) of times
-			SecureRandom.seedTime();
-			SecureRandom.seedInt8(evt.which);
-			var keyPressTimeDiff = timeStamp - ninja.seeder.lastInputTime;
-			SecureRandom.seedInt8(keyPressTimeDiff);
-			ninja.seeder.seedCount++;
-			ninja.seeder.lastInputTime = new Date().getTime();
-			ninja.seeder.showPool();
-		}
-	},
+		// seeding is over now we generate and display the address
+			if (ninja.seeder.seedCount == ninja.seeder.seedLimit) {
+				ninja.seeder.seedCount++;
+			ninja.wallets.singlewallet.open();
+				document.getElementById("generate").style.display = "none";
+				document.getElementById("menu").style.visibility = "visible";
+				ninja.seeder.removePoints();
+			}
+			// seed mouse position X and Y when mouse movements are greater than 40ms apart.
+			else if ((ninja.seeder.seedCount < ninja.seeder.seedLimit) && evt && (timeStamp - ninja.seeder.lastInputTime) > 40) {
+				SecureRandom.seedTime();
+				SecureRandom.seedInt16((evt.clientX * evt.clientY));
+				ninja.seeder.showPoint(evt.clientX, evt.clientY);
+				ninja.seeder.seedCount++;
+				ninja.seeder.lastInputTime = new Date().getTime();
+				ninja.seeder.showPool();
+			}
+		},
 
-	showPool: function () {
-		var poolHex;
-		if (SecureRandom.poolCopyOnInit != null) {
-			poolHex = Crypto.util.bytesToHex(SecureRandom.poolCopyOnInit);
-			document.getElementById("seedpool").innerHTML = poolHex;
-			document.getElementById("seedpooldisplay").innerHTML = poolHex;
-		}
-		else {
-			poolHex = Crypto.util.bytesToHex(SecureRandom.pool);
-			document.getElementById("seedpool").innerHTML = poolHex;
-			document.getElementById("seedpooldisplay").innerHTML = poolHex;
-		}
-		document.getElementById("mousemovelimit").innerHTML = (ninja.seeder.seedLimit - ninja.seeder.seedCount);
-	},
+		// seed function exists to wait for mouse movement to add more entropy before generating an address
+		seedKeyPress: function (evt) {
+			if (!evt) var evt = window.event;
+			// seeding is over now we generate and display the address
+			if (ninja.seeder.seedCount == ninja.seeder.seedLimit) {
+				ninja.seeder.seedCount++;
+			ninja.wallets.singlewallet.open();
+				document.getElementById("generate").style.display = "none";
+				document.getElementById("menu").style.visibility = "visible";
+				ninja.seeder.removePoints();
+			}
+			// seed key press character
+			else if ((ninja.seeder.seedCount < ninja.seeder.seedLimit) && evt.which) {
+				var timeStamp = new Date().getTime();
+				// seed a bunch (minimum seedLimit) of times
+				SecureRandom.seedTime();
+				SecureRandom.seedInt8(evt.which);
+				var keyPressTimeDiff = timeStamp - ninja.seeder.lastInputTime;
+				SecureRandom.seedInt8(keyPressTimeDiff);
+				ninja.seeder.seedCount++;
+				ninja.seeder.lastInputTime = new Date().getTime();
+				ninja.seeder.showPool();
+			}
+		},
 
-	showPoint: function (x, y) {
-		var div = document.createElement("div");
-		div.setAttribute("class", "seedpoint");
-		div.style.top = y + "px";
-		div.style.left = x + "px";
-		document.body.appendChild(div);
-		ninja.seeder.seedPoints.push(div);
-	},
+		showPool: function () {
+			var poolHex;
+			if (SecureRandom.poolCopyOnInit != null) {
+				poolHex = Crypto.util.bytesToHex(SecureRandom.poolCopyOnInit);
+				document.getElementById("seedpool").innerHTML = poolHex;
+				document.getElementById("seedpooldisplay").innerHTML = poolHex;
+			}
+			else {
+				poolHex = Crypto.util.bytesToHex(SecureRandom.pool);
+				document.getElementById("seedpool").innerHTML = poolHex;
+				document.getElementById("seedpooldisplay").innerHTML = poolHex;
+			}
+			document.getElementById("mousemovelimit").innerHTML = (ninja.seeder.seedLimit - ninja.seeder.seedCount);
+		},
 
-	removePoints: function () {
-		for (var i = 0; i < ninja.seeder.seedPoints.length; i++) {
-			document.body.removeChild(ninja.seeder.seedPoints[i]);
+		showPoint: function (x, y) {
+			var div = document.createElement("div");
+			div.setAttribute("class", "seedpoint");
+			div.style.top = y + "px";
+			div.style.left = x + "px";
+			
+			// let's make the entropy 'points' grow and change color!
+			percentageComplete = ninja.seeder.seedCount / ninja.seeder.seedLimit;
+			document.getElementById("progress-bar-percentage").style.width=Math.ceil(percentageComplete*100)+"%";
+
+			// for some reason, appending these divs to an IOS device breaks clicking altogether (?)
+			if (navigator.platform != 'iPad' && navigator.platform != 'iPhone' && navigator.platform != 'iPod') {
+				document.body.appendChild(div);
+			}
+			ninja.seeder.seedPoints.push(div);
+		},
+
+		removePoints: function () {
+			for (var i = 0; i < ninja.seeder.seedPoints.length; i++) {
+				document.body.removeChild(ninja.seeder.seedPoints[i]);
+			}
+			ninja.seeder.seedPoints = [];
 		}
-		ninja.seeder.seedPoints = [];
-	}
-};
+	};
 
 ninja.qrCode = {
 	// determine which type number is big enough for the input text length
