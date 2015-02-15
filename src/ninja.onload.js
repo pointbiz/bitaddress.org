b9a802163bcc81ab9be780b621357fdfc0a8c673
Michael Muré <mure.michael@gmail.com>
2015-02-15 22:01:22 +0100

    Clean some code that make crawlers unhappy

--- src/ninja.onload.js
+++ src/ninja.onload.js
@@ -51,10 +51,10 @@ var list = "<table>";
 for(i = 0; i < janin.currencies.length; i++) {
 	if(janin.currencies[i].donate == null)
 		continue;
-    list += "<tr onmouseover='ninja.wallets.donate.displayQrCode("+i+", this)'>"
-        +"<td class='currencyNameColumn'>"+janin.currencies[i].name+"</td>"
-        +"<td class='address'><a href='"+janin.currencies[i].name.toLowerCase()+":"+janin.currencies[i].donate+"'>"
-        +janin.currencies[i].donate+"</a></td></tr>";
+    list += "<tr onmouseover='ninja.wallets.donate.displayQrCode("+i+", this)'>";
+    list += "<td class='currencyNameColumn'>"+janin.currencies[i].name+"</td>";
+    list += "<td class='address'><a href='"+janin.currencies[i].name.toLowerCase()+":"+janin.currencies[i].donate+"'>";
+    list += janin.currencies[i].donate+"</a></td></tr>";
 }
 list += "</table>";
 donatelist.innerHTML = list;
