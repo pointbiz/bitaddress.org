fb86a83eb9591b6f6ffd8f4ead7ef3f4ba5b80f9
testzcrypto <testzcrypto@gmail.com>
2015-06-26 18:50:19 +0600

    Add support for EnergyCoin

--- src/janin.currency.js
+++ src/janin.currency.js
@@ -104,6 +104,7 @@ janin.currencies = [
     janin.currency.createCurrency ("eGulden",    0x30, 0xb0, "6",    "T"    , "LhBsKs2GUb24KBAzZfua5AsqfQF5uPdWXQ"),
     janin.currency.createCurrency ("eKrona",     0x2d, 0xad, "6",    "S"    , "KLi8FnMZmSH8EfXYgJwi4R2ZyMscJykXT5"),
     janin.currency.createCurrency ("EmerCoin",   0x21, 0xa1, "6",    "Q"    , "EN5nVyEbLrhYfcjoyGgQFtD3QHETyj1dy1"),
+    janin.currency.createCurrency ("EnergyCoin", 0x5c, 0xdc, "8",    "Z"    , "eD2P3q5PdyHYNwT94Dg6Wt4pBz64k8gwGf"),
     janin.currency.createCurrency ("Fastcoin",   0x60, 0xe0, "8",    "a"    , "frxe8F7gQdiAVgy4mRXjpXH5vN1wyta1db"),
     janin.currency.createCurrency ("Feathercoin",0x0e, 0x8e, "5",    "N"    , "6dxAP6oacHsove5X2kZPpddcT1Am167YzC"),
     janin.currency.createCurrency ("Fibre",      0x23, 0xa3, "6",    "R"    , "F6qGSM29vJm2q3Q9uvozpym7WYqKXBrpqm"),
