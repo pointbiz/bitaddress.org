da4e60c981ac4e413c1909c4822ddc6e71b06a8e
Michael Muré <batolettre@gmail.com>
2017-12-17 13:32:41 +0100

    Add support for TrezarCoin, contribution from fenrisl.

--- src/janin.currency.js
+++ src/janin.currency.js
@@ -234,6 +234,7 @@ janin.currencies = [
     janin.currency.createCurrency ("Topcoin",             0x42, 0xc2, "7",    "V"    , "TmDTsQqqv1LWGw4xjGNiJ7ABwdCenf2BFF"),
     janin.currency.createCurrency ("TransferCoin",        0x42, 0x99, "6",    "P"    , "TbnW6ih8314ksuutJpRjwUbc2mAkz64Tij"),
     janin.currency.createCurrency ("TreasureHuntCoin",    0x32, 0xb2, "6",    "T"    , "MKnC2upgCNfVMS2phkV8SqGaXUGkn39EaX"),
+    janin.currency.createCurrency ("TrezarCoin",          0x42, 0xC2, "9",    "V"    , "Tw1jsLJKfmcosUCkJuMevdLLJob9wD7PSE"),
     janin.currency.createCurrency ("Unobtanium",          0x82, 0xe0, "8",    "a"    , "uZ8Gq61NGJ2wz3PLybXyXKLYC1FhRpz8Kq"),
     janin.currency.createCurrency ("USDe",                0x26, 0xa6, "6",    "R"    , "GQTeNSfx6xPbBNsUfqoZNrrCBQXeY5Dtdu"),
     janin.currency.createCurrency ("Vcash",               0x47, 0xc7, "7",    "W"    , "VoaKH8ndxJoFfM3XJ7DK3P6g7kxASpCf5g"),
